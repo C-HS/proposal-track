66937b49e3570998ae2538f22f41ec72d10cc675
zafar <zafar.parvez@gmail.com>
2020-11-04 13:30:47 +0530

    Updatedd the Project.

--- src/main/java/com/indic/proposal/service/dto/ProposalDTO.java
+++ src/main/java/com/indic/proposal/service/dto/ProposalDTO.java
@@ -11,48 +11,20 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-    "proposalId",
-    "proposalDocument",
-    "shpcDocument",
-    "totalProposalCost",
-    "totalCentralShare",
-    "totalStateShare",
-    "totalOtherShare",
-    "component"
-})
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Entity
 public class ProposalDTO implements Serializable {
 
     private static final long serialVersionUID = -9057319436314533599L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonProperty("proposalId")
     public Long proposalId;
-    @JsonProperty("proposalDocument")
     public String proposalDocument;
-    @JsonProperty("shpcDocument")
     public String shpcDocument;
-    @JsonProperty("totalProposalCost")
     public String totalProposalCost;
-    @JsonProperty("totalCentralShare")
     public String totalCentralShare;
-    @JsonProperty("totalStateShare")
     public String totalStateShare;
-    @JsonProperty("totalOtherShare")
     public String totalOtherShare;
-    @JsonProperty("component")
-    @OneToMany(mappedBy = "proposal")
     public List<ComponentDTO> componentDTO = new ArrayList<>();
-
-    public void addComponent(ComponentDTO componentDTO){
-        this.componentDTO.add(componentDTO);
-        componentDTO.setProposal(this);
-    }
 }
--- src/main/java/com/indic/proposal/service/service/MediaService.java
+++ src/main/java/com/indic/proposal/service/service/MediaService.java
@@ -1,4 +1,14 @@
 package com.indic.proposal.service.service;
 
+import com.indic.proposal.service.dto.MediaDTO;
+import com.indic.proposal.service.model.Media;
+
+import java.util.List;
+
 public interface MediaService {
+    public MediaDTO addMedia(Media media);
+    public MediaDTO fetchMediaById(long mediaId);
+    public List<MediaDTO> fetchAllMedia();
+    public void deleteMediaById(long mediaId);
+    public MediaDTO updateMedia(long mediaId, Media media);
 }
